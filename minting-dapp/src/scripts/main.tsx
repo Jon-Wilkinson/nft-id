5e1a9b7bbe87191583e722d13c4bab565406e9b8
Jon <Jon.Wilkinson@rockets.utoledo.edu>
2022-04-06 12:08:26 -0400

    Combined App.tsx with main.tsx, removed App.tsx

--- minting-dapp/src/scripts/main.tsx
+++ minting-dapp/src/scripts/main.tsx
@@ -1,10 +1,17 @@
 import '../styles/main.scss';
 
 import ReactDOM from 'react-dom';
-import Dapp from './react/Dapp';
-import App from './App';
+import MainPage from './react/MainPage';
+import ExPage from './react/ExPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 document.addEventListener('DOMContentLoaded', async () => {
-  ReactDOM.render(<App/>, document.getElementById('app'));
-  // ReactDOM.render(<Dapp />, document.getElementById('minting-dapp'));
+  ReactDOM.render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage/>} />
+        <Route path="expage" element={<ExPage/>} />
+      </Routes>
+    </BrowserRouter>
+    , document.getElementById('app'));
 });
