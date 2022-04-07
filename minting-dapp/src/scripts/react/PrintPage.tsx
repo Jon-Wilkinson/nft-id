a11ae08e2ba23291aee21d51b61bf2a5b190e595
Jon <Jon.Wilkinson@rockets.utoledo.edu>
2022-04-07 14:12:02 -0400

    Removed web3-eth

--- minting-dapp/src/scripts/react/PrintPage.tsx
+++ minting-dapp/src/scripts/react/PrintPage.tsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const ContractAbi = require('../../../../smart-contract/artifacts/contracts/' + "Print" + '.sol/' + "Print" + '.json').abi;
 
 export default function ExPage() {
-  const Eth = require('web3-eth');
+
   return (
     <div>
       <h1>This is an example page</h1>
