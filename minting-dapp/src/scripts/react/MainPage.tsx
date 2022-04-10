1e7cf4e9be57966898d7ce652d711343a5014465
Jon <Jon.Wilkinson@rockets.utoledo.edu>
2022-04-10 11:12:40 -0400

    Mainpage now checks if the user is logged in

--- minting-dapp/src/scripts/react/MainPage.tsx
+++ minting-dapp/src/scripts/react/MainPage.tsx
@@ -1,33 +1,59 @@
+import React from 'react';
 import '../../styles/main.scss';
 import Dapp from '.././react/Dapp';
-import {Link} from 'react-router-dom';
 
-export default function MainPage() {
-  return (
-    <div>
-      <section id="left">
-      <h1>SENIOR DESIGN NFT PROJECT</h1>
-      <p>EECS 4020 </p>
-      <p>SPRING 2022</p>
-      <img id="logo" src="/build/images/logo.png" alt="Logo" />
-      </section>
-      <main>
-      
-      
-      {/* <hr/> */}
+interface Props {
 
-      <section id="right">
-      <div id="minting-dapp">
-        <Dapp/>
-      </div>
-      </section>
-      
-      </main>
-      
-      <footer>
-        <hr/>
-        <p> By Raymond Eleko, Josh  Spurgeon, Sydney Evans, Jon Wilkinson, Daniel Wellons </p>
-      </footer>
-  </div>
-  );
+}
+interface State {
+  loggedIn: boolean;
+}
+
+export default class MainPage extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    }
+  }
+
+  componentDidMount = async () => {
+    localStorage.getItem('username') == '' ?
+      this.setState({loggedIn: false}) : this.setState({loggedIn: true});
+  }
+
+  render() {
+    return (
+      this.state.loggedIn ?
+      <div>
+        <section id="left">
+        <h1>SENIOR DESIGN NFT PROJECT</h1>
+        <p>EECS 4020 </p>
+        <p>SPRING 2022</p>
+        <img id="logo" src="/build/images/logo.png" alt="Logo" />
+        </section>
+        <main>
+        
+        
+        {/* <hr/> */}
+
+        <section id="right">
+        <div id="minting-dapp">
+          <Dapp/>
+        </div>
+        </section>
+        
+        </main>
+        
+        <footer>
+          <hr/>
+          <p> By Raymond Eleko, Josh  Spurgeon, Sydney Evans, Jon Wilkinson, Daniel Wellons </p>
+        </footer>
+    </div>
+    :
+    <h1>
+      Error! You need to log in first
+    </h1>
+    );
+  }
 }
